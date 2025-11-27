1944f2d636792126bd360c5da07da2a6c5409e39
TRUYEN97 <samtruyen60@gmail.com>
2025-11-27 17:27:32 +0700

    add base

---
+++ src/i18n.ts
@@ -0,0 +1,34 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+const resources = {
+  en: {
+    translation: {
+      sign_up_page: {
+        title: "Sign Up"
+      }
+    }
+  },
+  vi: {
+    translation: {
+      sign_up_page: {
+        title: "Đăng nhập"
+      }
+    }
+  }
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "vi",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+  export default i18n;
---
+++ src/pages/AuthPages/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import AuthLayout from '../../layout/AuthLayout'
+import SignInForm from '../../components/auth/SignInForm'
+
+const Login = () => {
+
+    return (
+        <AuthLayout>
+            <SignInForm />
+        </AuthLayout>
+    )
+}
+
+export default Login
